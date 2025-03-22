8f46d55416aa420ad1e96885e11e43722ef222dd
Mark Navalta <marknavalta.3@gmail.com>
2025-03-22 10:28:53 -0500

    Install langchain, add venv to gitignore, import libraries

---
+++ main.py
@@ -0,0 +1,6 @@
+# Import necessary libraries
+from langchain.llms import HuggingFaceHub
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+
