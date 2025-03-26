d4ea721914d06be86bb81ba7cb32d649a385f587
Mark Navalta <marknavalta.3@gmail.com>
2025-03-26 14:35:52 -0500

    Added embeddings

--- tut.py
+++ tut.py
@@ -7,3 +7,7 @@ print(LANGSMITH_API_KEY)
 from langchain.chat_models import init_chat_model
 
 llm = init_chat_model("mistral-large-latest", model_provider="mistralai")
+
+from langchain_huggingface import HuggingFaceEmbeddings
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
