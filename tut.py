ca41677a2444a773373297e47d894cfc051c78d6
Mark Navalta <marknavalta.3@gmail.com>
2025-03-26 14:34:14 -0500

    llm

--- tut.py
+++ tut.py
@@ -3,3 +3,7 @@ with open("token.txt", "r") as token_file:
 HUGGINGFACEHUB_API_TOKEN = tokens.split()[0]
 LANGSMITH_API_KEY = tokens.split()[1]
 print(LANGSMITH_API_KEY)
+
+from langchain.chat_models import init_chat_model
+
+llm = init_chat_model("mistral-large-latest", model_provider="mistralai")
