0af356753193bbb6321d370510195e38bbdd5409
Mark Navalta <marknavalta.3@gmail.com>
2025-04-01 14:42:28 -0500

    Renamed main to rag, modulized rag, basic ui implementation

---
+++ rag.py
@@ -0,0 +1,76 @@
+def llm_init():
+    with open("token.txt", "r") as token_file:
+        tokens = token_file.read().strip()
+    HUGGINGFACEHUB_API_TOKEN = tokens.split()[0]
+    LANGSMITH_API_KEY = tokens.split()[1]
+
+    from langchain_huggingface import HuggingFaceEndpoint
+
+    repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+    llm = HuggingFaceEndpoint(
+        repo_id=repo_id,
+        temperature=0.5,
+        huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
+    )
+
+    from langchain_huggingface import HuggingFaceEmbeddings
+
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+
+    import faiss
+    from langchain_community.docstore.in_memory import InMemoryDocstore
+    from langchain_community.vectorstores import FAISS
+
+    embedding_dim = len(embeddings.embed_query("hello world"))
+    index = faiss.IndexFlatL2(embedding_dim)
+
+    vector_store = FAISS(
+        embedding_function=embeddings,
+        index=index,
+        docstore=InMemoryDocstore(),
+        index_to_docstore_id={},
+    )
+
+    # Load and chunk contents
+    from langchain_community.document_loaders import TextLoader
+    from langchain_text_splitters import RecursiveCharacterTextSplitter, MarkdownHeaderTextSplitter # try both
+
+    loader = TextLoader("./data/resume.md")
+    docs = loader.load()
+
+    print(f"Total characters: {len(docs[0].page_content)}")
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    all_splits = text_splitter.split_documents(docs)
+
+    print(f"Split resume into {len(all_splits)} sub-documents.")
+
+    # Store vectors
+    document_ids = vector_store.add_documents(documents=all_splits)
+
+    return llm, vector_store
+
+def get_answer(llm, question, vector_store):
+    # Retrieval and Generation
+    from langchain import hub
+
+    prompt = hub.pull("rlm/rag-prompt")
+
+    example_messages = prompt.invoke(
+        {"context": "(context goes here)", "question": "(question goes here)"}
+    ).to_messages()
+
+    assert len(example_messages) == 1
+    print(example_messages[0].content)
+
+    retrieved_docs = vector_store.similarity_search(question)
+    docs_content = "\n\n".join(doc.page_content for doc in retrieved_docs)
+    prompt = prompt.invoke({"question": question, "context": docs_content})
+    answer = llm.invoke(prompt)
+
+    print("Answer:", answer)
+    return answer
+
+# RAG: https://python.langchain.com/docs/tutorials/rag/
+
+
--- ui.py
+++ ui.py
@@ -1,1 +1,17 @@
 import streamlit as st
+from rag import llm_init, get_answer
+
+
+# Title for the portfolio chatbot
+st.title("Portfolio Chatbot")
+
+# Input area for the user's question
+user_question = st.text_input("Ask me anything about my experiences:")
+
+llm, vs = llm_init()
+
+# Button to submit the question
+if st.button("Submit") and user_question:
+    # Retrieve the answer using your RAG logic
+    answer = get_answer(llm, user_question, vs)  # function that wraps RAG code logic
+    st.write("Answer:", answer)
